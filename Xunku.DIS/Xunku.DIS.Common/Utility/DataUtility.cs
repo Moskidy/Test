1343551faf690f48755db3a6daacce93204612fe
徐侃 <mosquito_163@163.com>
2017-12-31 21:18:53 +0800

    项目源代码整理

---
+++ Xunku.DIS/Xunku.DIS.Common/Utility/DataUtility.cs
@@ -0,0 +1,63 @@
+﻿namespace Xunku.DIS.Common.Utility
+{
+    /// <summary>
+    /// 数据工具类
+    /// 用于数据格式之间的转换
+    /// </summary>
+    public static class DataUtility
+    {
+        /// <summary>
+        /// 布尔值转换
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="defaultValue">转换失败后的默认返回值</param>
+        /// <returns></returns>
+        public static bool? GetBool(string input, bool? defaultValue = null)
+        {
+            bool ret;
+
+            if (!bool.TryParse(input, out ret))
+            {
+                return defaultValue;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 有符号整数转换
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="defaultValue">转换失败后的默认返回值</param>
+        /// <returns></returns>
+        public static int? GetInt(string input, int? defaultValue = null)
+        {
+            int ret;
+
+            if (!int.TryParse(input, out ret))
+            {
+                return defaultValue;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 双精度浮点数转换
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="defaultValue">转换失败后的默认返回值</param>
+        /// <returns></returns>
+        public static double? GetDouble(string input, double? defaultValue = null)
+        {
+            double ret;
+
+            if (!double.TryParse(input, out ret))
+            {
+                return defaultValue;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ Xunku.DIS/Xunku.DIS.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Xunku.DIS.Common.Utility;
+
+namespace Xunku.DIS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var input = "123";
+            var ret = DataUtility.GetInt(input);
+
+            if (ret.HasValue)
+            {
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
